d0408278ab0c39b4c7332be9dfd09ed4e68f5788
Tiana Im <jihyung_im@apple.com>
2016-05-14 13:07:36 -0700

    removed mistakes

--- thankr/urls.py
+++ thankr/urls.py
@@ -21,8 +21,13 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login/', "thankr.accounts.views.login"),
+    url(r'^login/', "thankr.accounts.views.user_login", name='login'),
     url(r'^register/', "thankr.accounts.views.register"),
     url(r'^', TemplateView.as_view(template_name='index.html')),
     url(r'^moments/', include('thankr.moments.urls')),
 ]
+
+
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic import TemplateView
