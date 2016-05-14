9d3edff9216f95d22bb65f49dcda3777933af504
Tiana Im <jihyung_im@apple.com>
2016-05-14 12:12:57 -0700

    added registration view

--- thankr/accounts/views.py
+++ thankr/accounts/views.py
@@ -1,6 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.contrib.auth.models import User
+from django import forms
+from thankr.accounts.forms import UserForm
+from django.template import RequestContext
+
 
 # Create your views here.
 
 def login(request):
 	return render(request, "login.html")
+
+def register(request):
+    # Like before, get the request's context.
+    context = RequestContext(request)
+
+    # A boolean value for telling the template whether the registration was successful.
+    # Set to False initially. Code changes value to True when registration succeeds.
+    registered = False
+
+    # If it's a HTTP POST, we're interested in processing form data.
+    if request.method == 'POST':
+        # Attempt to grab information from the raw form information.
+        # Note that we make use of both UserForm.
+        user_form = UserForm(data=request.POST)
+
+        # If the two forms are valid...
+        if user_form.is_valid() and profile_form.is_valid():
+            # Save the user's form data to the database.
+            user = user_form.save()
+
+            # Now we hash the password with the set_password method.
+            # Once hashed, we can update the user object.
+            user.set_password(user.password)
+            user.save()
+
+            # Update our variable to tell the template registration was successful.
+            registered = True
+
+        # Invalid form or forms - mistakes or something else?
+        # Print problems to the terminal.
+        # They'll also be shown to the user.
+        else:
+            print user_form.errors, profile_form.errors
+
+    # Not a HTTP POST, so we render our form using two ModelForm instances.
+    # These forms will be blank, ready for user input.
+    else:
+        user_form = UserForm()
+
+    # Render the template depending on the context.
+    return render_to_response(
+            'register.html',
+            {'user_form': user_form, 'registered': registered},
+            context)
--- thankr/urls.py
+++ thankr/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', "thankr.accounts.views.login"),
+    url(r'^register/', "thankr.accounts.views.register"),
+    url(r'^', TemplateView.as_view(template_name='index.html'))
 ]
