796778fe34546532e212a2980bea35ef364ed149
dkt1412 <dkthung@gmail.com>
2016-05-14 13:46:57 -0700

    Added POST /moments to add moments

--- thankr/moments/resources/views.py
+++ thankr/moments/resources/views.py
@@ -1,15 +1,35 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import permissions, status
 
 from thankr.moments.resources.serializers import MomentSerializer
 from thankr.moments.models import Moment
 
 class MomentsResource(APIView):
-	#permission_classes = (permissions.IsAuthenticated,)
+	permission_classes = (permissions.IsAuthenticated,)
 
 	def get(self, request):
-		moments = Moment.objects.filter(user_id=1)
+		moments = Moment.objects.filter(user_id=request.user.id)
 
 		moments_serializer = MomentSerializer(moments, many=True)
 		return Response(moments_serializer.data, status=status.HTTP_200_OK)
+
+
+	def post(self, request):
+		user_id = request.user.id
+		try:
+			title = request.data['title']
+			text = request.data['text']
+		except KeyError:
+			return Response("parse error", status=status.HTTP_400_BAD_REQUEST)
+
+		category_id = request.data['category_id'] if 'category_id' in request.data else None
+		rating = request.data['rating'] if 'rating' in request.data else None
+
+		moment = Moment(user_id = user_id,
+						title = title,
+						text = text,
+						category_id = category_id,
+						rating = rating)
+		moment.save()
+		return Response(MomentSerializer(moment).data, status=status.HTTP_200_OK)
--- thankr/urls.py
+++ thankr/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', "thankr.accounts.views.user_login", name='login'),
     url(r'^register/', "thankr.accounts.views.register"),
-    url(r'^', TemplateView.as_view(template_name='index.html')),
     url(r'^moments/', include('thankr.moments.urls')),
+    url(r'^', TemplateView.as_view(template_name='index.html')),
 ]
