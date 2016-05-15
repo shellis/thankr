8b5c4a0b1d87069b1dbbcbd004c5dc184daad771
Shelli Skinner <sskinner413@gmail.com>
2016-05-14 17:10:51 -0700

    styled add moment and fixed data rendering issue

--- thankr/moments/resources/serializers.py
+++ thankr/moments/resources/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from thankr.moments.models import Moment
+from thankr.moments.models import Category, Moment
+
+class CategoryField(serializers.PrimaryKeyRelatedField):
+    def display_value(self, instance):
+        return instance.name
 
 class MomentSerializer(serializers.ModelSerializer):
+    category = CategoryField(queryset=Category.objects.all())
     class Meta:
         model = Moment
         fields = ('id', 'date', 'title', 'text', 'category', 'rating')
